2f6fc1e3d5c8b46907986ddd6f184226d417a90a
Patty <94728986+PattyHoswell@users.noreply.github.com>
2025-11-12 13:22:26 +0700

    Modded Compendium

---
+++ Patty_ModdedCompendium_MOD/ModdedCompendium.cs
@@ -0,0 +1,282 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using MelonLoader;
+using MelonLoader.Utils;
+using Patty_ModdedCompendium_MOD;
+using Patty_ModdedCompendium_MOD.Patch;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(ModdedCompendium), "Patty_ModdedCompendium_MOD", "2.0.0", "PattyHoswell")]
+[assembly: MelonGame("UmiArt", "Demon Bluff")]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.IL2CPP)]
+[assembly: MelonPriority(100)]
+[assembly: HarmonyDontPatchAll]
+namespace Patty_ModdedCompendium_MOD
+{
+    public class ModdedCompendium : MelonMod
+    {
+        public static readonly string MODDED_COMPENDIUM = "Modded_Compendium";
+        public static readonly string SORT_OPTION = "Sort_Option";
+        public static readonly string SHOW_UNCATEGORIZED_VANILLA_CHARS = "Show_Uncategorized_Vanilla_Characters";
+        public static MelonLogger.Instance Logger { get; internal set; }
+        public static readonly string COLOR_TAGS_REGEX = @"<color=.*?>";
+        public static readonly List<CharacterData> hiddenCharacterDatas = new List<CharacterData>();
+        public static readonly List<CharacterData> unknownCharacterDatas = new List<CharacterData>();
+        public static readonly Dictionary<MelonBase, List<CharacterData>> moddedCharacterDatasCategorized = new Dictionary<MelonBase, List<CharacterData>>();
+        public static readonly Dictionary<ECharacterType, List<CharacterData>> moddedCharacterDatas = new Dictionary<ECharacterType, List<CharacterData>>();
+        public static readonly Dictionary<ECharacterType, string> colorTags = new Dictionary<ECharacterType, string>
+        {
+            { ECharacterType.None, "<color=green>" },
+            { ECharacterType.Villager, "<color=lightblue>" },
+            { ECharacterType.Outcast, "<color=yellow>" },
+            { ECharacterType.Minion, "<color=orange>" },
+            { ECharacterType.Demon, "<color=red>" },
+        };
+        internal static MelonPreferences_Category configCategory;
+        public override void OnLateInitializeMelon()
+        {
+            Logger = LoggerInstance;
+            moddedCharacterDatas.Populate();
+
+            configCategory = MelonPreferences.CreateCategory(MODDED_COMPENDIUM);
+            configCategory.CreateEntry(SORT_OPTION, ECategorySortOption.Regular, description: "Define the sort options. Valid Options:" +
+                                                                        $"\n{string.Join(", ", Enum.GetNames(typeof(ECategorySortOption)))}");
+            configCategory.CreateEntry(SHOW_UNCATEGORIZED_VANILLA_CHARS, true, description: "When enabled, show characters that are in-game but are hidden");
+            configCategory.SetFilePath(Path.Combine(MelonEnvironment.UserDataDirectory, "ModdedCompendium.cfg"));
+            configCategory.SaveToFile();
+            try
+            {
+                HarmonyInstance.PatchAll(typeof(PatchList));
+            }
+            catch (HarmonyException ex)
+            {
+                LoggerInstance.BigError(ex.ToString());
+            }
+        }
+
+        public static MelonBase GetMelonBaseFromRole(Role role)
+        {
+            if (role == null)
+            {
+                return null;
+            }
+            return MelonBase.RegisteredMelons.FirstOrDefault(x => x.MelonAssembly.Location == role.GetType().Assembly.Location);
+        }
+
+        internal static IEnumerable<CharactersCompendiumPage> GetCompendiumCharacters(Compendium compendium, ECharacterType characterType)
+        {
+            Func<CharactersCompendiumPage, bool> findSimilarPage = x => x.pageName.Contains(characterType.GetName(), StringComparison.OrdinalIgnoreCase);
+            return compendium.pages.Where(findSimilarPage);
+        }
+
+        internal static void CreateNewPage(Compendium compendium, ECharacterType characterType, List<CharacterData> characterDatas)
+        {
+            if (characterDatas.Count <= 0)
+            {
+                return;
+            }
+            Func<CharactersCompendiumPage, bool> findSimilarPage = x => x.pageName.Contains(characterType.GetName(), StringComparison.OrdinalIgnoreCase);
+            var charactersPages = GetCompendiumCharacters(compendium, characterType);
+            var pagesCount = charactersPages.Count();
+            var page = charactersPages.FirstOrDefault(x => x.characterDatas.Length < compendium.cards.Length) ?? new CharactersCompendiumPage();
+            if (page.characterDatas == null)
+            {
+                page.characterDatas = new CharacterData[0];
+            }
+            var availableSpace = compendium.cards.Length - page.characterDatas.Length;
+            var sliceCount = Mathf.Min(availableSpace, characterDatas.Count);
+            var slicedCharacterDatas = characterDatas.GetRange(0, Mathf.Max(sliceCount, 0));
+            characterDatas.RemoveRange(0, slicedCharacterDatas.Count);
+            page.characterDatas = page.characterDatas.Union(slicedCharacterDatas).ToArray();
+
+            var colorTag = "";
+            var firstItem = charactersPages.FirstOrDefault();
+            if (firstItem != null)
+            {
+                var match = Regex.Match(charactersPages.First().pageName, COLOR_TAGS_REGEX);
+                if (match.Success)
+                {
+                    colorTag = match.Groups[0].Value;
+                }
+            }
+            else if ((firstItem == null || string.IsNullOrWhiteSpace(colorTag)) && !colorTags.TryGetValue(characterType, out colorTag))
+            {
+                Logger.Warning($"Unable to get color tag for {characterType.GetName()}");
+            }
+            if (!compendium.pages.Contains(page))
+            {
+                var lastIndex = compendium.pages.FindLastIndex(findSimilarPage);
+                compendium.pages = lastIndex >= 0 ? compendium.pages.Insert(lastIndex + 1, page) : compendium.pages.Append(page).ToArray();
+                page.pageName = $"{colorTag}{characterType.GetName()}s";
+                if (pagesCount >= 1)
+                {
+                    page.pageName += $" {pagesCount}/{pagesCount + 1}";
+                }
+            }
+
+            if (characterDatas.Count > 0)
+            {
+                CreateNewPage(compendium, characterType, characterDatas);
+            }
+        }
+
+        internal static void CreateNewPageCategorized(Compendium compendium, MelonBase melonBase, List<CharacterData> characterDatas)
+        {
+            if (characterDatas.Count <= 0)
+            {
+                return;
+            }
+            var nameSearch = melonBase != null ? melonBase.Info.Name : "Unknown";
+            Func<CharactersCompendiumPage, bool> findSimilarPage = x => x.pageName.Contains(nameSearch, StringComparison.OrdinalIgnoreCase);
+            var modPages = compendium.pages.Where(findSimilarPage);
+            var page = modPages.FirstOrDefault(x => x.characterDatas.Length < compendium.cards.Length) ?? new CharactersCompendiumPage();
+            if (page.characterDatas == null)
+            {
+                page.characterDatas = new CharacterData[0];
+            }
+            var availableSpace = compendium.cards.Length - page.characterDatas.Length;
+            var sliceCount = Mathf.Min(availableSpace, characterDatas.Count);
+            var slicedCharacterDatas = characterDatas.GetRange(0, Mathf.Max(sliceCount, 0));
+            characterDatas.RemoveRange(0, slicedCharacterDatas.Count);
+
+            page.characterDatas = page.characterDatas.Union(slicedCharacterDatas).ToArray();
+
+            var colorTag = "";
+            if (!colorTags.TryGetValue(ECharacterType.None, out colorTag))
+            {
+                Logger.Warning($"Unable to get color tag for {ECharacterType.None.GetName()}");
+            }
+            if (!compendium.pages.Contains(page))
+            {
+                compendium.pages = compendium.pages.Append(page).ToArray();
+            }
+            page.pageName = nameSearch;
+
+            if (characterDatas.Count > 0)
+            {
+                CreateNewPageCategorized(compendium, melonBase, characterDatas);
+            }
+        }
+
+        internal static void SortCompendium(Compendium compendium, bool rename = true)
+        {
+            var villagersCategory = GetCompendiumCharacters(compendium, ECharacterType.Villager).ToArray();
+            var outcastsCategory = GetCompendiumCharacters(compendium, ECharacterType.Outcast).ToArray();
+            var minionsCategory = GetCompendiumCharacters(compendium, ECharacterType.Minion).ToArray();
+            var demonsCategory = GetCompendiumCharacters(compendium, ECharacterType.Demon).ToArray();
+            var unknownCategory = compendium.pages.Except(demonsCategory).Except(outcastsCategory).Except(minionsCategory).Except(demonsCategory).ToArray();
+            Sort(villagersCategory, ECharacterType.Villager, rename);
+            Sort(outcastsCategory, ECharacterType.Outcast, rename);
+            Sort(minionsCategory, ECharacterType.Minion, rename);
+            Sort(demonsCategory, ECharacterType.Demon, rename);
+            if (unknownCategory != null && unknownCategory.Length > 0)
+            {
+                SortCategorized(compendium, unknownCategory, rename);
+            }
+        }
+        static void Sort(CharactersCompendiumPage[] category, ECharacterType characterType, bool rename = true)
+        {
+            for (var i = 0; i < category.Length; i++)
+            {
+                var item = category[i];
+                if (!string.IsNullOrWhiteSpace(item.pageName))
+                {
+                    var colorTag = "";
+                    var match = Regex.Match(item.pageName, COLOR_TAGS_REGEX);
+                    if (match.Success)
+                    {
+                        colorTag = match.Groups[0].Value;
+                    }
+                    else if (!colorTags.TryGetValue(characterType, out colorTag))
+                    {
+                        Logger.Warning($"Unable to get color tag for {characterType.GetName()}");
+                    }
+                    if (rename)
+                    {
+                        item.pageName = $"{colorTag}{characterType.GetName()}s";
+                        if (category.Length > 1)
+                        {
+                            item.pageName += $" {i + 1}/{category.Length}";
+                        }
+                    }
+                }
+                else
+                {
+                    var colorTag = "";
+                    if (!colorTags.TryGetValue(characterType, out colorTag))
+                    {
+                        Logger.Warning($"Unable to get color tag for {characterType.GetName()}");
+                    }
+                    if (rename)
+                    {
+                        item.pageName = $"{colorTag}{characterType.GetName()}s";
+                        if (category.Length > 1)
+                        {
+                            item.pageName += $" {i + 1}/{category.Length}";
+                        }
+                    }
+                }
+                if (item.characterDatas != null)
+                {
+                    item.characterDatas = item.characterDatas.OrderBy(x => x.type).ThenBy(x => x.startingAlignment).ThenBy(x => x.name).ToArray();
+                }
+            }
+        }
+
+        static void SortCategorized(Compendium compendium, CharactersCompendiumPage[] category, bool rename = true)
+        {
+            for (var i = 0; i < category.Length; i++)
+            {
+                var item = category[i];
+                Func<CharactersCompendiumPage, bool> findSimilarPage = x => x.pageName.Equals(item.pageName, StringComparison.OrdinalIgnoreCase);
+                var modPages = compendium.pages.Where(findSimilarPage);
+                if (!string.IsNullOrWhiteSpace(item.pageName))
+                {
+                    var colorTag = "";
+                    var match = Regex.Match(item.pageName, COLOR_TAGS_REGEX);
+                    if (match.Success)
+                    {
+                        colorTag = match.Groups[0].Value;
+                    }
+                    else if (!colorTags.TryGetValue(ECharacterType.None, out colorTag))
+                    {
+                        Logger.Warning($"Unable to get color tag for {ECharacterType.None.GetName()}");
+                    }
+                    if (rename)
+                    {
+                        item.pageName = $"{colorTag}{item.pageName}";
+                        if (modPages.Count() > 1)
+                        {
+                            item.pageName += $" {modPages.FindIndex(item) + 1}/{modPages.Count()}";
+                        }
+                    }
+                }
+                else
+                {
+                    var colorTag = "";
+                    if (!colorTags.TryGetValue(ECharacterType.None, out colorTag))
+                    {
+                        Logger.Warning($"Unable to get color tag for {ECharacterType.None.GetName()}");
+                    }
+                    if (rename)
+                    {
+                        item.pageName = $"{colorTag}{item.pageName}";
+                        if (modPages.Count() > 1)
+                        {
+                            item.pageName += $" {modPages.FindIndex(item) + 1}/{modPages.Count()}";
+                        }
+                    }
+                }
+                if (item.characterDatas != null)
+                {
+                    item.characterDatas = item.characterDatas.OrderBy(x => x.type).ThenBy(x => x.startingAlignment).ThenBy(x => x.name).ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ Patty_ModdedCompendium_MOD/Patch/PatchList.cs
@@ -0,0 +1,128 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace Patty_ModdedCompendium_MOD.Patch
+{
+    internal static class PatchList
+    {
+        public static Lazy<Il2CppArrayBase<CharacterData>> allCharacterData = new Lazy<Il2CppArrayBase<CharacterData>>(() => Resources.FindObjectsOfTypeAll<CharacterData>(), isThreadSafe: false);
+
+
+        [HarmonyPrefix, HarmonyPatch(typeof(Compendium), nameof(Compendium.LoadUnlockedCharacters))]
+        public static void LoadUnlockedCharacters(Compendium __instance)
+        {
+            if (__instance.unlockedCharacters == null)
+            {
+                __instance.unlockedCharacters = new Il2CppSystem.Collections.Generic.List<CharacterData>();
+            }
+            bool hasAddedAnything = false;
+            Dictionary<ECharacterType, List<CharacterData>> newHiddenChars = new Dictionary<ECharacterType, List<CharacterData>>();
+            List<CharacterData> newUnknownChars = new List<CharacterData>();
+            Dictionary<MelonBase, List<CharacterData>> newMelonBases = new Dictionary<MelonBase, List<CharacterData>>();
+            Dictionary<ECharacterType, List<CharacterData>> newCharTypes = new Dictionary<ECharacterType, List<CharacterData>>();
+            foreach (var characterData in allCharacterData.Value)
+            {
+                if (characterData == null)
+                {
+                    continue;
+                }
+                if (ModdedCompendium.moddedCharacterDatas.HasValue(characterData))
+                {
+                    continue;
+                }
+                hasAddedAnything = true;
+                if (Path.GetFileName(characterData.role?.GetType().Assembly.Location) == "Assembly-CSharp.dll")
+                {
+                    if (ModdedCompendium.configCategory.GetEntry<bool>(ModdedCompendium.SHOW_UNCATEGORIZED_VANILLA_CHARS).Value == true &&
+                        __instance.pages.All(x => x.characterDatas.All(y => y != characterData)))
+                    {
+                        newHiddenChars.AddToList(characterData.type, characterData);
+                        ModdedCompendium.hiddenCharacterDatas.Add(characterData);
+                    }
+                    continue;
+                }
+                var melonBase = ModdedCompendium.GetMelonBaseFromRole(characterData.role);
+                if (melonBase != null)
+                {
+                    newMelonBases.AddToList(melonBase, characterData);
+                    ModdedCompendium.moddedCharacterDatasCategorized.AddToList(melonBase, characterData);
+                }
+                else
+                {
+                    newUnknownChars.Add(characterData);
+                    ModdedCompendium.unknownCharacterDatas.Add(characterData);
+                }
+                ModdedCompendium.moddedCharacterDatas.AddToList(characterData.type, characterData);
+                newCharTypes.AddToList(characterData.type, characterData);
+            }
+            if (!hasAddedAnything)
+            {
+                return;
+            }
+            if (ModdedCompendium.configCategory.GetEntry<bool>(ModdedCompendium.SHOW_UNCATEGORIZED_VANILLA_CHARS).Value == true)
+            {
+                foreach (var (characterType, characterDatas) in newHiddenChars)
+                {
+                    if (characterDatas.Count > 0)
+                    {
+                        ModdedCompendium.CreateNewPage(__instance, characterType, characterDatas);
+                    }
+                }
+            }
+            if (ModdedCompendium.configCategory.GetEntry<ECategorySortOption>(ModdedCompendium.SORT_OPTION).Value == ECategorySortOption.ByMod)
+            {
+                foreach (var (melonBase, characterDatas) in newMelonBases)
+                {
+                    if (characterDatas.Count > 0)
+                    {
+                        ModdedCompendium.CreateNewPageCategorized(__instance, melonBase, characterDatas);
+                    }
+                }
+                if (newUnknownChars.Count > 0)
+                {
+                    ModdedCompendium.CreateNewPageCategorized(__instance, null, newUnknownChars);
+                }
+            }
+            else
+            {
+                foreach (var (characterType, characterDatas) in newCharTypes)
+                {
+                    if (characterDatas.Count > 0)
+                    {
+                        ModdedCompendium.CreateNewPage(__instance, characterType, characterDatas);
+                    }
+                }
+            }
+            ModdedCompendium.SortCompendium(__instance);
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(Compendium), nameof(Compendium.LoadUnlockedCharacters))]
+        public static void LoadUnlockedCharacters_Postfix(Compendium __instance)
+        {
+            if (__instance.unlockedCharacters == null)
+            {
+                __instance.unlockedCharacters = new Il2CppSystem.Collections.Generic.List<CharacterData>();
+            }
+            foreach (var characterData in ModdedCompendium.hiddenCharacterDatas)
+            {
+                if (!__instance.unlockedCharacters.Contains(characterData))
+                    __instance.unlockedCharacters.Add(characterData);
+            }
+            foreach (var characterList in ModdedCompendium.moddedCharacterDatas.Values)
+            {
+                foreach (var characterData in characterList)
+                    if (!__instance.unlockedCharacters.Contains(characterData))
+                        __instance.unlockedCharacters.Add(characterData);
+            }
+            __instance.allCharacters = __instance.unlockedCharacters.Count;
+            __instance.UpdateCompletion();
+        }
+    }
+}
